b021e24110565fd9b2e594368451de41a434e6b9
jihyun2ya <jihyun2yo@gmail.com>
2018-11-09 01:55:40 +0900

    Task3

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
+import React, {Component} from 'react';
+import {Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle} from "reactstrap";
 
 class DishDetail extends Component {
 
@@ -8,18 +8,37 @@ class DishDetail extends Component {
     }
 
     render() {
-        if (this.props.selectedDish != null)
-            return (
-                <Card>
-                    <CardImg top src={this.props.selectedDish.image} alt={this.props.selectedDish.name} />
-                    <CardBody>
-                        <CardTitle>{this.props.selectedDish.name}</CardTitle>
-                        <CardText>{this.props.selectedDish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
+
+
+        if (this.props.selectedDish != null) {
+            const comments = this.props.selectedDish.comments.map((item) => {
+                return (
+                    <div key={item.id}>
+                        {item.comment}<br/><br/>
+                        --{item.author}, {item.date}<br/><br/>
+                    </div>
+                );
+            });
+
             return(
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={this.props.selectedDish.image} alt={this.props.selectedDish.name}/>
+                            <CardBody>
+                                <CardTitle>{this.props.selectedDish.name}</CardTitle>
+                                <CardText>{this.props.selectedDish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h1>Comments</h1>
+                        {comments}
+                    </div>
+                </div>
+                );
+        } else
+            return (
                 <div></div>
             );
     }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -43,11 +43,8 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
-                        <DishDetail selectedDish={this.state.selectedDish}/>
-                    </div>
-                </div>
+                <DishDetail selectedDish={this.state.selectedDish}/>
+
             </div>
         );
     }
